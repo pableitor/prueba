9003979acd9c4478a150b370c6314505f23c6be4
Pablo <torrentep@gmail.com>
2019-12-12 11:40:11 +0100

    fix counter bug

--- hola.py
+++ hola.py
@@ -6,6 +6,6 @@ Created on Thu Dec 12 10:10:21 2019
 """
 
 print("Hola Mundo!")
+x = input("pulsa una tecla para continuar...")
 for i in range(1,11):
     print(i)
-x = input("pulsa una tecla para terminar...")
